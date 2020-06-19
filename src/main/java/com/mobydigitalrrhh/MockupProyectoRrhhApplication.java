dc200ecbee1270c5f0a77de2fa9343d0b9b0991a
agustindegiorgi <agustindegiorgi@users.noreply.github.com>
2020-06-19 10:59:20 -0300

    Prueba de conexión en Heroku

--- src/main/java/com/mobydigitalrrhh/MockupProyectoRrhhApplication.java
+++ src/main/java/com/mobydigitalrrhh/MockupProyectoRrhhApplication.java
@@ -2,6 +2,8 @@ package com.mobydigitalrrhh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MockupProyectoRrhhApplication {
@@ -9,5 +11,12 @@ public class MockupProyectoRrhhApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MockupProyectoRrhhApplication.class, args);
 	}
-
+	
+	@RestController
+	public class HolaController {
+	    @GetMapping("/")
+	    public String index() {
+	        return "Hello there! I'm running.";
+	    }
+	}
 }

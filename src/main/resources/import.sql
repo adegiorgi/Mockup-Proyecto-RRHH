ee8cf49e3aee1913f9dbadc1b5750e9acc0aea95
norbertomagarzo <norbertomagarzo@gmail.com>
2020-06-23 11:50:49 -0300

    H2 insert Borrados

--- src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `personas` (`id_persona`, `apellido`, `ciudad`, `conocimiento`, `dni`, `habilidades`, `idioma`, `nombre`, `seniority`, `tecnica`, `viajar`) VALUES ('1', 'De Giorgi', 'Córdoba', '1', '43369999', '3', 'Español', 'Agustín', 'Trainee', '2', 'true');
-INSERT INTO `personas` (`id_persona`, `apellido`, `ciudad`, `conocimiento`, `dni`, `habilidades`, `idioma`, `nombre`, `seniority`, `tecnica`, `viajar`) VALUES ('2', 'Magarzo', 'Córdoba', '2', '34483920', '2', 'Español', 'Norberto', 'Trainee', '1', 'false');
-
-INSERT INTO `tecnologias` (`id_tecnologia`, `nombre`) VALUES ('1', 'Java');
-INSERT INTO `tecnologias` (`id_tecnologia`, `nombre`) VALUES ('2', 'NodeJS');
-INSERT INTO `tecnologias` (`id_tecnologia`, `nombre`) VALUES ('3', 'Angular');
-INSERT INTO `tecnologias` (`id_tecnologia`, `nombre`) VALUES ('4', 'Ux Designer');
-INSERT INTO `tecnologias` (`id_tecnologia`, `nombre`) VALUES ('5', 'JavaScript');
